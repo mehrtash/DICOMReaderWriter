e6f771a7580db952793b52ca1faf552db337a7ed
Ubuntu 64 <alz@ubuntu.(none)>
2013-11-14 10:58:41 -0500

    change the reader type to gdcm

--- DICOMReaderWriter.cxx
+++ DICOMReaderWriter.cxx
@@ -6,7 +6,18 @@
 #include "itkDICOMImageIO2.h"
 #include "itkDICOMSeriesFileNames.h"
 #include "itkImageFileWriter.h"
-
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkExtractImageFilter.h"
+#include "itkShiftScaleImageFilter.h"
+#include "itkGDCMImageIO.h"
+#include "itkMetaDataObject.h"
+#include "itkGDCMSeriesFileNames.h"
+#include "itkNumericSeriesFileNames.h"
+#include "itkImageSeriesReader.h"
+#include <vector>
+#include "itksys/SystemTools.hxx"
+#include "itkPluginUtilities.h"
 
 namespace
 {
@@ -17,62 +28,50 @@ namespace
 int main(int argc, char* argv[])
 {
   PARSE_ARGS;
-   typedef itk::Image<short,3>                  ImageType;
+
+  typedef itk::Image<short,3>                  ImageType;
   typedef itk::ImageSeriesReader< ImageType >  ReaderType;
 
-  itk::DICOMImageIO2::Pointer dicomIO = itk::DICOMImageIO2::New();
+  typedef itk::GDCMImageIO                        ImageIOType;
+  typedef itk::GDCMSeriesFileNames                NamesGeneratorType;
 
-  // Get the DICOM filenames from the directory
-  itk::DICOMSeriesFileNames::Pointer nameGenerator = itk::DICOMSeriesFileNames::New();
-  nameGenerator->SetDirectory(inputDirectory);
+  typedef itk::Image<ImageType, 3>         Image3DType;
+  typedef itk::Image<ImageType, 2>         Image2DType;
 
+  //typedef itk::ImageFileWriter<Image2DType>     WriterType;
+
+  typedef itk::ImageFileWriter< ImageType > WriterType;
+
+  WriterType::Pointer writer = WriterType::New();
+
+  ImageIOType::Pointer gdcmIO = ImageIOType::New();
+
+  // Reading the input DICOCM directory and get the
+  // attribute values which needs to be copied to the
+  //output DICOM directory
+
+  NamesGeneratorType::Pointer namesGenerator = NamesGeneratorType::New();
+  namesGenerator->SetInputDirectory( inputDirectory );
+
+  const ReaderType::FileNamesContainer & filenames =
+                            namesGenerator->GetInputFileNames();
+
+  ReaderType::Pointer reader = ReaderType::New();
+
+  reader->SetImageIO( gdcmIO );
+  reader->SetFileNames( filenames );
 
   try
     {
-    typedef std::vector<std::string> seriesIdContainer;
-    const seriesIdContainer & seriesUID = nameGenerator->GetSeriesUIDs();
+    reader->Update();
+    }
 
-    seriesIdContainer::const_iterator seriesItr = seriesUID.begin();
-    seriesIdContainer::const_iterator seriesEnd = seriesUID.end();
-
-    std::cout << std::endl << "The directory: " << std::endl;
-    std::cout << std::endl << inputDirectory << std::endl << std::endl;
-    std::cout << "Contains the following DICOM Series: ";
-    std::cout << std::endl << std::endl;
-
-    while( seriesItr != seriesEnd )
-      {
-      std::cout << seriesItr->c_str() << std::endl;
-      ++seriesItr;
-      }
-
-    std::cout << std::endl << std::endl;
-    std::cout << "Now reading series: " << std::endl << std::endl;
-
-    typedef std::vector<std::string> fileNamesContainer;
-    fileNamesContainer fileNames;
-
-
-
-    ReaderType::Pointer reader = ReaderType::New();
-    reader->SetFileNames( fileNames );
-    reader->SetImageIO( dicomIO );
-
-    try
-      {
-      reader->Update();
-      }
-    catch (itk::ExceptionObject &ex)
-      {
-      std::cout << ex << std::endl;
-      return EXIT_FAILURE;
-      }
-
-    typedef itk::ImageFileWriter< ImageType > WriterType;
-    WriterType::Pointer writer = WriterType::New();
-
-    std::cout  << "Writing the image as " << std::endl << std::endl;
-    std::cout  << outputVolume << std::endl << std::endl;
+  catch (itk::ExceptionObject &excp)
+    {
+    std::cerr << "Exception thrown while writing the image" << std::endl;
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
 
     writer->SetFileName( outputVolume.c_str() );
 
@@ -87,12 +86,8 @@ int main(int argc, char* argv[])
       std::cout << ex;
       return EXIT_FAILURE;
       }
-    }
-  catch (itk::ExceptionObject &ex)
-    {
-    std::cout << ex;
-    return EXIT_FAILURE;
-    }
+
+
 
   return EXIT_SUCCESS;
 }
