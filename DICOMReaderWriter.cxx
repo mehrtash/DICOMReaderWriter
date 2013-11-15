df9ad47ef54752e921c00b24437e96ac5e3c6c67
Ubuntu 64 <alz@ubuntu.(none)>
2013-11-15 12:46:52 -0500

    first commit

--- DICOMReaderWriter.cxx
+++ DICOMReaderWriter.cxx
@@ -3,6 +3,7 @@
 
 // ITK includes
 #include "itkImageSeriesReader.h"
+#include "itkImageSeriesWriter.h"
 #include "itkDICOMImageIO2.h"
 #include "itkDICOMSeriesFileNames.h"
 #include "itkImageFileWriter.h"
@@ -30,19 +31,25 @@ int main(int argc, char* argv[])
 {
   PARSE_ARGS;
 
-  typedef itk::Image<short,3>                  ImageType;
-  typedef itk::ImageSeriesReader< ImageType >  ReaderType;
+  typedef itk::Image<short,3>                  Image3DType;
+  typedef itk::ImageSeriesReader< Image3DType >  ReaderType;
 
   typedef itk::GDCMImageIO            gdcmIOType;
   typedef itk::DCMTKImageIO           dcmtkIOType;        
   typedef itk::GDCMSeriesFileNames    gdcmNamesGeneratorType;
   typedef itk::DCMTKSeriesFileNames   dcmtkNamesGeneratorType;
 
-  typedef itk::ImageFileWriter< ImageType > WriterType;
+  typedef itk::ImageFileWriter< Image3DType > WriterType;
   
   ReaderType::Pointer reader = ReaderType::New();
   WriterType::Pointer writer = WriterType::New();
 
+  typedef signed short OutputPixelType;
+  const unsigned int OutputDimension = 2;
+
+  typedef itk::Image< OutputPixelType, OutputDimension> image2DType;
+
+  //GDCM Reader-Writer
   if( ioType == "GDCM" )
   {
     gdcmIOType::Pointer gdcmIO = gdcmIOType::New();
@@ -64,7 +71,31 @@ int main(int argc, char* argv[])
       std::cerr << excp << std::endl;
       return EXIT_FAILURE;
     }
+
+    typedef itk::ImageSeriesWriter<
+            Image3DType,image2DType> SeriesWriterType;
+    SeriesWriterType::Pointer seriesWriter = SeriesWriterType::New();
+    seriesWriter->SetInput(reader->GetOutput());
+    seriesWriter->SetImageIO(gdcmIO);
+    gdcmNamesGenerator->SetOutputDirectory( outputDirectory);
+    seriesWriter->SetFileNames(gdcmNamesGenerator->GetOutputFileNames());
+    seriesWriter->SetMetaDataDictionaryArray(
+                            reader->GetMetaDataDictionaryArray() );
+    try
+       {
+       seriesWriter->Update();
+       }
+     catch( itk::ExceptionObject & excp )
+       {
+       std::cerr << "Exception thrown while writing the series " << std::endl;
+       std::cerr << excp << std::endl;
+       return EXIT_FAILURE;
+       }
+
+
   }
+
+  //DCMTK Reader-Writer
   else //DCMTK
   {
     dcmtkIOType::Pointer dcmtkIO = dcmtkIOType::New();
@@ -89,16 +120,17 @@ int main(int argc, char* argv[])
     }
   }
 
-    writer->SetFileName( outputVolume.c_str() );
-    writer->SetInput( reader->GetOutput() );
-    try
-    {
-      writer->Update();
-    }
-    catch (itk::ExceptionObject &ex)
-    {
-      std::cout << ex;
-      return EXIT_FAILURE;
-    }
+  writer->SetFileName( outputVolume.c_str() );
+  writer->SetInput( reader->GetOutput() );
+  try
+  {
+    writer->Update();
+  }
+  catch (itk::ExceptionObject &ex)
+  {
+    std::cout << ex;
+    return EXIT_FAILURE;
+  }
+
   return EXIT_SUCCESS;
 }
